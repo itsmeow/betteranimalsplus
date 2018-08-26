580227299172447d94bb98d9e5ade9bd9af9c834
BuildTools <mods.itsmeow@gmail.com>
2018-08-26 16:39:16 -0400

    Fix textures

--- java/its_meow/betteranimalsplus/registry/TextureRegistry.java
+++ java/its_meow/betteranimalsplus/registry/TextureRegistry.java
@@ -10,11 +10,11 @@ public class TextureRegistry {
 	
 	//Bear Variants
 	public static final ResourceLocation bear_brown = 
-			new ResourceLocation(entitytex, "bear_brown.png");
+			new ResourceLocation(entitytex + "bear_brown.png");
 	public static final ResourceLocation bear_black = 
-			new ResourceLocation(entitytex, "bear_black.png");
+			new ResourceLocation(entitytex + "bear_black.png");
 	public static final ResourceLocation bear_kermode = 
-			new ResourceLocation(entitytex, "bear_kermode.png");
+			new ResourceLocation(entitytex + "bear_kermode.png");
 	
 	
 }
