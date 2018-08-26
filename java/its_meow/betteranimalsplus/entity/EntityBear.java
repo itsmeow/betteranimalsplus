19a87f849d417e40b964ea139362b77d607242a5
BuildTools <mods.itsmeow@gmail.com>
2018-08-26 16:36:51 -0400

    Fix attr bug 2

--- java/its_meow/betteranimalsplus/entity/EntityBear.java
+++ java/its_meow/betteranimalsplus/entity/EntityBear.java
@@ -69,7 +69,6 @@ public class EntityBear extends EntityMob {
 		this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(30.0D);
 		this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(20.0D);
 		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.25D);
-		this.getAttributeMap().registerAttribute(SharedMonsterAttributes.ATTACK_DAMAGE);
 		this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(6.0D);
 	}
 
