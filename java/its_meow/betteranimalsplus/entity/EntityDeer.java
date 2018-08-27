f9d14142a5b8b459e8ae400071897d01679a8b6e
BuildTools <mods.itsmeow@gmail.com>
2018-08-26 21:56:34 -0400

    Reduce deer speed

--- java/its_meow/betteranimalsplus/entity/EntityDeer.java
+++ java/its_meow/betteranimalsplus/entity/EntityDeer.java
@@ -32,12 +32,12 @@ public class EntityDeer extends EntityAnimal {
 	{
 		super.initEntityAI();
 		this.tasks.addTask(0, new EntityAISwimming(this));
-        this.tasks.addTask(1, new EntityAIPanic(this, 0.8D));
-        this.tasks.addTask(2, new EntityAIAvoidEntity<EntityPlayer>(this, EntityPlayer.class, 20, 0.6D, 0.7D));
-        this.tasks.addTask(3, new EntityAIWander(this, 0.45D));
+        this.tasks.addTask(2, new EntityAIPanic(this, 0.65D));
+        this.tasks.addTask(3, new EntityAIAvoidEntity<EntityPlayer>(this, EntityPlayer.class, 20, 0.55D, 0.65D));
+        this.tasks.addTask(4, new EntityAIWander(this, 0.45D));
         this.eatGrassAI = new EntityAIEatGrass(this);
-        this.tasks.addTask(4, eatGrassAI);
-        this.tasks.addTask(5, new EntityAILookIdle(this));
+        this.tasks.addTask(5, eatGrassAI);
+        this.tasks.addTask(6, new EntityAILookIdle(this));
 	}
 
 	protected void applyEntityAttributes()
