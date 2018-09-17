1ec3a1dd959f553282904bce6fb8282cc0187215
Ri5ux <Ri5ux@users.noreply.github.com>
2018-09-17 11:42:19 -0500

    Removed unused imports.

--- src/main/java/its_meow/betteranimalsplus/proxy/ClientProxy.java
+++ src/main/java/its_meow/betteranimalsplus/proxy/ClientProxy.java
@@ -6,8 +6,6 @@ import its_meow.betteranimalsplus.registry.MobRegistry;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ClientProxy extends CommonProxy {
 	
