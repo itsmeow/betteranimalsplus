00d7b412bd5c415ee68658285a1b41aa39e10028
BuildTools <mods.itsmeow@gmail.com>
2018-09-18 21:01:00 -0400

    Add trillium item texture and hirschgeist skull texture

--- src/main/java/its_meow/betteranimalsplus/block/render/RenderBlockHirschgeistSkull.java
+++ src/main/java/its_meow/betteranimalsplus/block/render/RenderBlockHirschgeistSkull.java
@@ -3,9 +3,12 @@ package its_meow.betteranimalsplus.block.render;
 import its_meow.betteranimalsplus.block.TileEntityHirschgeistSkull;
 import its_meow.betteranimalsplus.entity.model.ModelHirschgeistSkull;
 import its_meow.betteranimalsplus.registry.TextureRegistry;
+import net.minecraft.block.BlockDirectional;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.fml.relauncher.Side;
 
@@ -26,7 +29,21 @@ public class RenderBlockHirschgeistSkull extends TileEntitySpecialRenderer<TileE
 		GlStateManager.pushMatrix();
         GlStateManager.translate(x+ 0.5F,y + 1.5F, z + 0.5F);
         GlStateManager.rotate(180, 0, 0, 1);
-        this.bindTexture(TextureRegistry.hirschgeistskull);
+        World world = tileentity.getWorld();
+        Comparable facing = world.getBlockState(tileentity.getPos()).getProperties().get(BlockDirectional.FACING);
+        if(facing == EnumFacing.NORTH) {
+        	
+        }
+        if(facing == EnumFacing.EAST) {
+        	
+        }
+        if(facing == EnumFacing.WEST) {
+        	
+        }
+        if(facing == EnumFacing.SOUTH) {
+        	
+        }
+        this.bindTexture(TextureRegistry.hirschgeist);
         this.mainModel.render((Entity) null, 0F, 0F, 0F, 0F, 0F, 0.0625F);
         GlStateManager.popMatrix();
     }
