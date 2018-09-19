06b57ae1a097a45210a51444c746357a7a307121
BuildTools <mods.itsmeow@gmail.com>
2018-09-19 19:40:18 -0400

    Add wearable skull

--- src/main/java/its_meow/betteranimalsplus/item/ItemHirschgeistSkull.java
+++ src/main/java/its_meow/betteranimalsplus/item/ItemHirschgeistSkull.java
@@ -1,12 +1,18 @@
 package its_meow.betteranimalsplus.item;
 
 import its_meow.betteranimalsplus.block.TileEntityHirschgeistSkull;
+import its_meow.betteranimalsplus.entity.model.ModelHirschgeistSkull;
+import its_meow.betteranimalsplus.entity.model.ModelHirschgeistSkullArmorPiece;
+import its_meow.betteranimalsplus.proxy.ClientProxy;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockSkull;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.model.ModelBiped;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
 import net.minecraft.item.ItemSkull;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
@@ -16,6 +22,8 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemHirschgeistSkull extends ItemBlockBase {
 
@@ -72,5 +80,45 @@ public class ItemHirschgeistSkull extends ItemBlockBase {
 	public boolean isValidArmor(ItemStack stack, EntityEquipmentSlot armorType, Entity entity) {
 		return armorType == EntityEquipmentSlot.HEAD;
 	}
+
+
+
+	@Override 	
+	@SideOnly(Side.CLIENT)
+	public ModelBiped getArmorModel(EntityLivingBase entityLiving, ItemStack itemStack, EntityEquipmentSlot armorSlot,
+			ModelBiped defaultModel) {
+
+		if (itemStack != null) {
+			if (itemStack.getItem() instanceof ItemArmor) {
+
+				EntityEquipmentSlot type = ((ItemArmor) itemStack.getItem()).armorType;
+				ModelBiped armorModel = null;
+				if(type == type.HEAD) {
+					armorModel = ClientProxy.helmetModel;
+					armorModel.bipedHead.showModel = armorSlot == EntityEquipmentSlot.HEAD;
+					armorModel.bipedHeadwear.showModel = armorSlot == EntityEquipmentSlot.HEAD;
+					armorModel.bipedBody.showModel = (armorSlot == EntityEquipmentSlot.CHEST)
+							|| (armorSlot == EntityEquipmentSlot.CHEST);
+					armorModel.bipedRightArm.showModel = armorSlot == EntityEquipmentSlot.CHEST;
+					armorModel.bipedLeftArm.showModel = armorSlot == EntityEquipmentSlot.CHEST;
+					armorModel.bipedRightLeg.showModel = (armorSlot == EntityEquipmentSlot.LEGS)
+							|| (armorSlot == EntityEquipmentSlot.FEET);
+					armorModel.bipedLeftLeg.showModel = (armorSlot == EntityEquipmentSlot.LEGS)
+							|| (armorSlot == EntityEquipmentSlot.FEET);
+
+					armorModel.isSneak = defaultModel.isSneak;
+					armorModel.isRiding = defaultModel.isRiding;
+					armorModel.isChild = defaultModel.isChild;
+					armorModel.rightArmPose = defaultModel.rightArmPose;
+					armorModel.leftArmPose = defaultModel.leftArmPose;
+
+					return armorModel;
+				}
+			}
+		}
+		return null;
+	}
+
+
 
 }
--- src/main/java/its_meow/betteranimalsplus/proxy/ClientProxy.java
+++ src/main/java/its_meow/betteranimalsplus/proxy/ClientProxy.java
@@ -4,12 +4,16 @@ import its_meow.betteranimalsplus.block.TileEntityHirschgeistSkull;
 import its_meow.betteranimalsplus.block.TileEntityTrillium;
 import its_meow.betteranimalsplus.block.render.RenderBlockHirschgeistSkull;
 import its_meow.betteranimalsplus.block.render.RenderBlockTrillium;
+import its_meow.betteranimalsplus.entity.model.ModelHirschgeistSkullArmorPiece;
+import its_meow.betteranimalsplus.registry.BlockRegistry;
 import its_meow.betteranimalsplus.registry.MobRegistry;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class ClientProxy extends CommonProxy {
+	
+	public static final ModelHirschgeistSkullArmorPiece helmetModel = new ModelHirschgeistSkullArmorPiece(0.0625F);
 	
 	public void preInit(FMLPreInitializationEvent event) {
 		super.preInit(event);
