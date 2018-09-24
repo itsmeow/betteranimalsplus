b825ef06cdcc4ee052541543d9fe2cca1ec3b1b0
BuildTools <mods.itsmeow@gmail.com>
2018-09-24 06:01:33 -0400

    HOF Recipe - Final 1.0 commit

--- src/main/java/its_meow/betteranimalsplus/registry/CraftingRegistry.java
+++ src/main/java/its_meow/betteranimalsplus/registry/CraftingRegistry.java
@@ -1,5 +1,7 @@
 package its_meow.betteranimalsplus.registry;
 
+import its_meow.betteranimalsplus.Ref;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.Item;
@@ -7,8 +9,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.oredict.OreIngredient;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 import net.minecraftforge.oredict.ShapelessOreRecipe;
 
@@ -16,5 +21,14 @@ public class CraftingRegistry {
 
 	public static final void register() {
 		GameRegistry.addSmelting(new ItemStack(ItemRegistry.venisonRaw), new ItemStack(ItemRegistry.venisonCooked), 0.0F);
+		for(ItemStack stack : OreDictionary.getOres("stone")) {
+			GameRegistry.addShapedRecipe(new ResourceLocation("handoffate"), new ResourceLocation(Ref.MOD_ID), new ItemStack(ItemRegistry.itemHandOfFate, 1), new Object[]{
+					"ibi",
+					" i ",
+					"sss",
+					Character.valueOf('i'), Blocks.IRON_BARS, 
+					Character.valueOf('b'), Items.BLAZE_POWDER, 
+					Character.valueOf('s'), stack});
+		}
 	}
 }
