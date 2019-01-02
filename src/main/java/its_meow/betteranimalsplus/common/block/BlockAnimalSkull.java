346e8327a37f15f4c2a35a77cfd45932b68d353a
itsmeow <mods.itsmeow@gmail.com>
2019-01-01 19:24:38 -0500

    Fix drops bug

--- src/main/java/its_meow/betteranimalsplus/common/block/BlockAnimalSkull.java
+++ src/main/java/its_meow/betteranimalsplus/common/block/BlockAnimalSkull.java
@@ -2,17 +2,22 @@ package its_meow.betteranimalsplus.common.block;
 
 import java.util.Random;
 
+import javax.annotation.Nullable;
+
 import its_meow.betteranimalsplus.BetterAnimalsPlusMod;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockSkull;
 import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.stats.StatList;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
@@ -21,24 +26,12 @@ import net.minecraft.world.World;
 public class BlockAnimalSkull extends BlockSkull implements ITileEntityProvider {
 
 	public BlockAnimalSkull() {
-		this.setHardness(1.0F);
+		this.setHardness(0.8F);
 		this.setSoundType(SoundType.STONE);
 		this.translucent = true;
 		this.fullBlock = false;
 		this.setCreativeTab(BetterAnimalsPlusMod.tab);
 	}
-	/**
-     * Called before the Block is set to air in the world. Called regardless of if the player's tool can actually
-     * collect this block
-     */
-    public void onBlockHarvested(World worldIn, BlockPos pos, IBlockState state, EntityPlayer player)
-    {
-        if (!player.capabilities.isCreativeMode)
-        {
-            Block.spawnAsEntity(worldIn, pos, new ItemStack(this.getItemBlock(), 1));
-        }
-        super.onBlockHarvested(worldIn, pos, state, player);
-    }
 
 	@Override
 	public boolean hasTileEntity() {
@@ -85,14 +78,30 @@ public class BlockAnimalSkull extends BlockSkull implements ITileEntityProvider 
 	}
 
 	@Override
-	public void getDrops(net.minecraft.util.NonNullList<ItemStack> drops, IBlockAccess worldIn, BlockPos pos, IBlockState state, int fortune)
+	public void getDrops(NonNullList<ItemStack> drops, IBlockAccess world, BlockPos pos, IBlockState state, int fortune) {
+		if (!((Boolean)state.getValue(NODROP)).booleanValue()) {
+			System.out.println("drop 1");
+			Item item = this.getItemBlock();
+			if (item != null && item != Items.AIR) {
+				drops.add(new ItemStack(item));
+			}
+		}
+	}
+
+	/**
+	 * Spawns the block's drops in the world. By the time this is called the Block has possibly been set to air via
+	 * Block.removedByPlayer
+	 */
+	public void harvestBlock(World worldIn, EntityPlayer player, BlockPos pos, IBlockState state, @Nullable TileEntity te, ItemStack stack)
 	{
-		drops.add(new ItemStack(this.getItemBlock(), 1));
+		player.addStat(StatList.getBlockStats(this));
+		player.addExhaustion(0.005F);
 	}
 
 	/**
 	 * Get the Item that this Block should drop when harvested.
 	 */
+	@Override
 	public Item getItemDropped(IBlockState state, Random rand, int fortune)
 	{
 		return this.getItemBlock();
