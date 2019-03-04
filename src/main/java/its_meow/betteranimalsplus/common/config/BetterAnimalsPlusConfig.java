bba47d70ce1d8b8be9f20c3a2f61fe558bf02193
itsmeow <mods.itsmeow@gmail.com>
2019-03-04 16:39:42 -0500

    Start new configs

--- src/main/java/its_meow/betteranimalsplus/common/config/BetterAnimalsPlusConfig.java
+++ src/main/java/its_meow/betteranimalsplus/common/config/BetterAnimalsPlusConfig.java
@@ -1,6 +1,30 @@
 package its_meow.betteranimalsplus.common.config;
 
+import java.nio.file.Path;
+
+import com.electronwill.nightconfig.core.file.CommentedFileConfig;
+import com.electronwill.nightconfig.core.io.WritingMode;
+
+import its_meow.betteranimalsplus.BetterAnimalsPlusMod;
+import its_meow.betteranimalsplus.Ref;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.Logging;
+import net.minecraftforge.fml.config.ModConfig;
+
+@Mod.EventBusSubscriber
 public class BetterAnimalsPlusConfig {
+
+	private static final ForgeConfigSpec.Builder SERVER_BUILDER = new ForgeConfigSpec.Builder();
+	//private static final ForgeConfigSpec.Builder CLIENT_BUILDER = new ForgeConfigSpec.Builder();
+
+	public static final ForgeConfigSpec SERVER_CONFIG;
+	//public static final ForgeConfigSpec CLIENT_CONFIG;
+	
+	static {
+        SERVER_CONFIG = SERVER_BUILDER.build();
+        //CLIENT_CONFIG = CLIENT_BUILDER.build();
+	}
 
 	public static int brownBearWeight = 7;
 	public static int blackBearWeight = 5;
@@ -20,6 +44,29 @@ public class BetterAnimalsPlusConfig {
 	public static int squirrelWeight = 8;
 
 	public static boolean spawnTrillium = true;
+	
+	
+	public static void loadConfig(ForgeConfigSpec spec, Path path) {
+        final CommentedFileConfig configData = CommentedFileConfig.builder(path)
+                .sync()
+                .autosave()
+                .writingMode(WritingMode.REPLACE)
+                .build();
+
+        configData.load();
+        spec.setConfig(configData);
+    }
+
+    @SubscribeEvent
+    public static void onLoad(final ModConfig.Loading configEvent) {
+        BetterAnimalsPlusMod.logger.debug("Loading {} {}", Ref.MOD_ID, configEvent.getConfig().getFileName());
+
+    }
+
+    @SubscribeEvent
+    public static void onFileChange(final ModConfig.ConfigReloading configEvent) {
+        BetterAnimalsPlusMod.logger.fatal(Logging.CORE, "{} changed incorrectly!", Ref.MOD_ID);
+    }
 
 	/*
 	 * public static HashMap<EntityContainer, EntityConfigurationSection>
