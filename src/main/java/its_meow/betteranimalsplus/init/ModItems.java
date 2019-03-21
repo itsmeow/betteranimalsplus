9e21793f7197e917b5702a35bb45e548f9238258
itsmeow <mods.itsmeow@gmail.com>
2019-03-21 01:41:48 -0400

    Remove more useless crap

--- src/main/java/its_meow/betteranimalsplus/init/ModItems.java
+++ src/main/java/its_meow/betteranimalsplus/init/ModItems.java
@@ -3,9 +3,7 @@ package its_meow.betteranimalsplus.init;
 import java.util.LinkedHashMap;
 
 import its_meow.betteranimalsplus.BetterAnimalsPlusMod;
-import its_meow.betteranimalsplus.common.item.ItemAntler;
 import its_meow.betteranimalsplus.common.item.ItemBetterFood;
-import its_meow.betteranimalsplus.common.item.ItemGoatCheese;
 import its_meow.betteranimalsplus.common.item.ItemHirschgeistSkullWearable;
 import net.minecraft.entity.Entity;
 import net.minecraft.init.Items;
@@ -20,9 +18,9 @@ public class ModItems {
 	public static final ItemBetterFood VENISON_RAW = new ItemBetterFood("venisonraw", 4, 0, 32, true);
 	public static final ItemBetterFood VENISON_COOKED = new ItemBetterFood("venisoncooked", 8, 1.2F, 32, true);
 	public static final ItemHirschgeistSkullWearable HIRSCHGEIST_SKULL_WEARABLE = new ItemHirschgeistSkullWearable();
-	public static final ItemAntler ANTLER = new ItemAntler();
+	public static final Item ANTLER = new Item(new Properties().group(BetterAnimalsPlusMod.group)).setRegistryName("antler");
 	public static final Item GOAT_MILK = new ItemBucketMilk(new Properties().containerItem(Items.BUCKET).group(BetterAnimalsPlusMod.group).maxStackSize(1)).setRegistryName("goatmilk");
-	public static final ItemGoatCheese GOAT_CHEESE = new ItemGoatCheese();
+	public static final ItemBetterFood GOAT_CHEESE = new ItemBetterFood("goatcheese", 3, 1, 15, false);
 	public static final ItemBetterFood PHEASANT_RAW = new ItemBetterFood("pheasantraw", 3, 0, 32, true);
 	public static final ItemBetterFood PHEASANT_COOKED = new ItemBetterFood("pheasantcooked", 7, 1.2F, 32, true);
 	
