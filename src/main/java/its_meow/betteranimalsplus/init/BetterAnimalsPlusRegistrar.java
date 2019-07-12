3fefdea4d559079f9b0b74c40be8b541b76160d4
itsmeow <mods.itsmeow@gmail.com>
2019-07-12 13:34:59 -0400

    Actually register crab meat

--- src/main/java/its_meow/betteranimalsplus/init/BetterAnimalsPlusRegistrar.java
+++ src/main/java/its_meow/betteranimalsplus/init/BetterAnimalsPlusRegistrar.java
@@ -70,7 +70,7 @@ public class BetterAnimalsPlusRegistrar {
 
         registry.registerAll(ModItems.VENISON_RAW, ModItems.VENISON_COOKED, ModItems.HIRSCHGEIST_SKULL_WEARABLE,
         ModItems.ANTLER, ModItems.GOAT_MILK, ModItems.GOAT_CHEESE, ModItems.PHEASANT_RAW,
-        ModItems.PHEASANT_COOKED, ModItems.WOLF_CAPE_CLASSIC, ModItems.WOLF_CAPE_TIMBER, ModItems.WOLF_CAPE_BLACK, ModItems.RECORD_CRAB_RAVE);
+        ModItems.PHEASANT_COOKED, ModItems.WOLF_CAPE_CLASSIC, ModItems.WOLF_CAPE_TIMBER, ModItems.WOLF_CAPE_BLACK, ModItems.CRAB_MEAT_RAW, ModItems.CRAB_MEAT_COOKED, ModItems.RECORD_CRAB_RAVE);
 
         for (EntityContainer<?> ent : ModEntities.entityList) {
             ItemBetterAnimalsPlusEgg egg = new ItemBetterAnimalsPlusEgg(ModEntities.getEntityType(ent.entityName),
