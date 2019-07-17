603ee3384f4098c31991577113956cab628c71e3
itsmeow <mods.itsmeow@gmail.com>
2019-07-17 01:07:22 -0400

    I hate sharks

--- src/main/java/its_meow/betteranimalsplus/common/entity/EntityShark.java
+++ src/main/java/its_meow/betteranimalsplus/common/entity/EntityShark.java
@@ -4,27 +4,29 @@ import java.util.Random;
 
 import javax.annotation.Nullable;
 
-import com.google.common.base.Predicates;
-
+import its_meow.betteranimalsplus.common.entity.ai.EntityAIMoveTowardsAttackTarget;
+import its_meow.betteranimalsplus.util.PolarVector3D;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.IEntityLivingData;
 import net.minecraft.entity.SharedMonsterAttributes;
-import net.minecraft.entity.ai.EntityAIAttackMelee;
-import net.minecraft.entity.ai.EntityAIHurtByTarget;
-import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAIFindEntityNearest;
 import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.DifficultyInstance;
-import net.minecraft.world.EnumDifficulty;
 import net.minecraft.world.World;
 
 public class EntityShark extends EntitySharkBase implements IVariantTypes {
 
     protected static final DataParameter<Integer> TYPE_NUMBER = EntityDataManager.<Integer>createKey(EntityShark.class, DataSerializers.VARINT);
     private float lastAttack = 0;
+    private float lastGrab = 0;
 
     public EntityShark(World world) {
         super(world);
@@ -34,13 +36,11 @@ public class EntityShark extends EntitySharkBase implements IVariantTypes {
     @Override
     protected void initEntityAI() {
         super.initEntityAI();
-        
-        if (this.getEntityWorld().getDifficulty() != EnumDifficulty.PEACEFUL) {
-            this.tasks.addTask(0, new EntityAIAttackMelee(this, 0.8D, true));
-            this.tasks.addTask(1, new EntityAIWander(this, 0.55D));
-            this.targetTasks.addTask(0, new EntityAIHurtByTarget(this, false, new Class[0]));
-            this.targetTasks.addTask(1, new EntityAINearestAttackableTarget<EntityLivingBase>(this, EntityLivingBase.class, 90, false, false, Predicates.alwaysTrue()));
-        }
+        this.tasks.addTask(0, new EntityAIMoveTowardsAttackTarget(this, 0.8D, 40F));
+        this.tasks.addTask(1, new EntityAIWatchClosest(this, EntityLiving.class, 15F));
+        this.tasks.addTask(2, new EntityAIWander(this, 0.55D));
+        //this.targetTasks.addTask(0, new EntityAIHurtByTarget(this, false, new Class[0]));
+        this.targetTasks.addTask(1, new EntityAIFindEntityNearest(this, EntityLiving.class));
     }
 
     @Override
@@ -51,21 +51,62 @@ public class EntityShark extends EntitySharkBase implements IVariantTypes {
         this.getAttributeMap().registerAttribute(SharedMonsterAttributes.ATTACK_DAMAGE);
         this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(8D);
     }
-    
+
     @Override
     public void onLivingUpdate() {
         super.onLivingUpdate();
         if(!this.world.isRemote && this.getAttackTarget() != null && !this.getAttackTarget().isDead) {
             if(this.getPassengers().contains(this.getAttackTarget())) {
-                float time = 10F; 
-                time *= 2F * (Math.random() + 1F);
+                float time = 20F; 
+                time *= 5F * (Math.random() + 1F);
                 if(this.lastAttack + time < this.ticksExisted) {
-                    this.attackEntityAsMob(this.getAttackTarget());
+                    //this.attackEntityAsMob(this.getAttackTarget());
                 }
-            } else if(this.getDistanceSq(this.getAttackTarget()) < 6) {
-                this.getAttackTarget().startRiding(this);
+            } else if(lastGrab  + 60F < this.ticksExisted && this.getDistanceSq(this.getAttackTarget()) < 10) {
+                this.getAttackTarget().startRiding(this, true);
+                lastGrab = this.ticksExisted;
             }
         }
+    }
+
+    @Override
+    public void updatePassenger(Entity passenger) {
+        if(this.isPassenger(passenger)) {
+            BlockPos pos = this.fromPolarCoordinates(new PolarVector3D(this.rotationYaw, 0F, 2F));
+            passenger.setPosition(this.posX + this.motionX + pos.getX(), this.posY - (this.height / 2) + this.motionY, this.posZ + this.motionZ + pos.getZ());
+            this.motionY += Math.abs(passenger.motionY);
+            if (passenger instanceof EntityLivingBase && (this.getAttackTarget() == null || this.getAttackTarget() != passenger)) {
+                this.setAttackTarget((EntityLivingBase) passenger);
+            }
+            if (this.world.isRemote) {
+                this.applyOrientationToEntity(passenger);
+            }
+        }
+    }
+
+    public BlockPos fromPolarCoordinates(PolarVector3D polar) {
+        double r = polar.getR();
+        double lat = polar.getThetaY() / 360;
+        double lon = polar.getThetaX() / 360;
+        double x = r * Math.sin(lat) * Math.cos(lon);
+        double y = r * Math.sin(lat) * Math.sin(lon);
+        double z = r * Math.cos(lat);
+        return new BlockPos(x, y, z);
+    }
+
+    @Override
+    public boolean canRiderInteract() {
+        return true;
+    }
+
+    @Override
+    public boolean shouldRiderSit() {
+        return false;
+    }
+
+    @Override
+    public boolean shouldDismountInWater(Entity entity) {
+        return false;
     }
 
     protected void entityInit() {
