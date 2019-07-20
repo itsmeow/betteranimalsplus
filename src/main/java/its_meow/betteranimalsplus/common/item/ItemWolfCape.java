23f8c171151bf28bf3bc5f125459be5b5c823ba2
itsmeow <mods.itsmeow@gmail.com>
2019-07-20 10:58:24 -0400

    Add pelts as repair item for cape

--- src/main/java/its_meow/betteranimalsplus/common/item/ItemWolfCape.java
+++ src/main/java/its_meow/betteranimalsplus/common/item/ItemWolfCape.java
@@ -2,11 +2,13 @@ package its_meow.betteranimalsplus.common.item;
 
 import its_meow.betteranimalsplus.BetterAnimalsPlusMod;
 import its_meow.betteranimalsplus.client.model.ModelWolfCape;
+import its_meow.betteranimalsplus.init.ModItems;
 import its_meow.betteranimalsplus.util.ArmorMaterialWolfCape;
 import net.minecraft.client.renderer.entity.model.BipedModel;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ArmorItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -41,6 +43,18 @@ public class ItemWolfCape extends ArmorItem {
             }
         }
         return super.getArmorModel(entityLiving, itemStack, armorSlot, defaultModel);
+    }
+
+    @Override
+    public boolean getIsRepairable(ItemStack toRepair, ItemStack repair) {
+        Item matchPelt = null;
+        switch(this.variant) {
+        case 1: matchPelt = ModItems.WOLF_PELT_SNOWY; break;
+        case 2: matchPelt = ModItems.WOLF_PELT_TIMBER; break;
+        case 3: matchPelt = ModItems.WOLF_PELT_BLACK; break;
+        default: return false;
+        }
+        return repair.getItem() == matchPelt;
     }
 
 }
