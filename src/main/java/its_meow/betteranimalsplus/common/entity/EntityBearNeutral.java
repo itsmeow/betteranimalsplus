359dc4fbf2f1061bdc47a8bdd2390ada632e44a8
itsmeow <mods.itsmeow@gmail.com>
2019-07-26 22:50:49 -0400

    Fix bear head drops

--- src/main/java/its_meow/betteranimalsplus/common/entity/EntityBearNeutral.java
+++ src/main/java/its_meow/betteranimalsplus/common/entity/EntityBearNeutral.java
@@ -117,7 +117,7 @@ public class EntityBearNeutral extends EntityBear implements IVariantTypes {
     public void doDropHead() {
         if (!world.isRemote && !this.isChild()) {
             if (this.rand.nextInt(12) == 0) {
-                ItemStack stack = new ItemStack(HeadTypes.BEARHEAD.getItem(this.getTypeNumber() + 1));
+                ItemStack stack = new ItemStack(HeadTypes.BEARHEAD.getItem(this.getTypeNumber()));
                 this.entityDropItem(stack, 0.5F);
             }
         }
