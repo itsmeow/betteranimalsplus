b85bbf0d6983bec7f1e693fbc4839aeea6e6a620
itsmeow <mods.itsmeow@gmail.com>
2019-08-19 08:49:54 -0400

    Make entities killed by lampreys not drop items (Resolves #74)

--- src/main/java/its_meow/betteranimalsplus/common/CommonEventHandler.java
+++ src/main/java/its_meow/betteranimalsplus/common/CommonEventHandler.java
@@ -7,6 +7,7 @@ import its_meow.betteranimalsplus.Ref;
 import its_meow.betteranimalsplus.common.entity.EntityBear;
 import its_meow.betteranimalsplus.common.entity.EntityBoar;
 import its_meow.betteranimalsplus.common.entity.EntityCrab;
+import its_meow.betteranimalsplus.common.entity.EntityLamprey;
 import its_meow.betteranimalsplus.common.entity.EntitySquirrel;
 import its_meow.betteranimalsplus.init.ModItems;
 import its_meow.betteranimalsplus.init.ModLootTables;
@@ -14,6 +15,7 @@ import its_meow.betteranimalsplus.init.ModTriggers;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.PolarBearEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.particles.ParticleTypes;
@@ -27,6 +29,7 @@ import net.minecraft.world.storage.loot.TableLootEntry;
 import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.event.entity.player.AttackEntityEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -130,6 +133,13 @@ public class CommonEventHandler {
     public static void onPlayerRespawn(PlayerEvent.Clone event) {
         if(event.getOriginal().getEntityData().contains("betteranimalsplus", Constants.NBT.TAG_COMPOUND)) {
             event.getPlayer().getEntityData().put("betteranimalsplus", event.getOriginal().getEntityData().getCompound("betteranimalsplus"));
+        }
+    }
+    
+    @SubscribeEvent
+    public static void onLivingDrop(LivingDropsEvent event) {
+        if(event.getSource().getTrueSource() instanceof EntityLamprey && !(event.getEntity() instanceof PlayerEntity)) {
+            event.getDrops().clear();
         }
     }
 
