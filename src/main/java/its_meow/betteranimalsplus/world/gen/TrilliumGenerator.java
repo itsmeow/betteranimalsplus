7b832379a49c7bdec1202e4ceed24b6c564f0b05
itsmeow <mods.itsmeow@gmail.com>
2019-11-12 10:58:47 -0500

    Test new webhook

--- src/main/java/its_meow/betteranimalsplus/world/gen/TrilliumGenerator.java
+++ src/main/java/its_meow/betteranimalsplus/world/gen/TrilliumGenerator.java
@@ -44,23 +44,4 @@ public class TrilliumGenerator implements IWorldGenerator {
         }
     }
 
-    /*
-     * public boolean generate(World worldIn, Random rand, BlockPos position) {
-     * for (int i = 0; i < 64; ++i)
-     * {
-     * BlockPos blockpos = position.add(rand.nextInt(8) - rand.nextInt(8),
-     * rand.nextInt(4) - rand.nextInt(4), rand.nextInt(8) - rand.nextInt(8));
-     * 
-     * if (worldIn.isAirBlock(blockpos) && (!worldIn.provider.isNether() ||
-     * blockpos.getY() < 255) && this.trillium.canBlockStay(worldIn, blockpos,
-     * this.state))
-     * {
-     * worldIn.setBlockState(blockpos, this.state, 2);
-     * }
-     * }
-     * 
-     * return true;
-     * }
-     */
-
 }
