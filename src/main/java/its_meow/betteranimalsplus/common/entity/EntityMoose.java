7f1b6fe751d079d5db47e7140259998435c223be
itsmeow <mods.itsmeow@gmail.com>
2019-11-21 23:38:09 -0500

    Tweak moose AI (reduce knockback, pushing, break lilypads, water movement)

--- src/main/java/its_meow/betteranimalsplus/common/entity/EntityMoose.java
+++ src/main/java/its_meow/betteranimalsplus/common/entity/EntityMoose.java
@@ -6,6 +6,12 @@ import its_meow.betteranimalsplus.common.entity.ai.EntityAIEatGrassCustom;
 import its_meow.betteranimalsplus.init.ModEntities;
 import its_meow.betteranimalsplus.util.EntityTypeContainer;
 import its_meow.betteranimalsplus.util.HeadTypes;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.crash.CrashReport;
+import net.minecraft.crash.CrashReportCategory;
+import net.minecraft.crash.ReportedException;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ILivingEntityData;
 import net.minecraft.entity.LivingEntity;
@@ -21,6 +27,8 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.Direction;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.IWorld;
@@ -30,13 +38,14 @@ public class EntityMoose extends EntityAnimalEatsGrassWithTypes {
 
     public EntityMoose(World worldIn) {
         super(ModEntities.MOOSE.entityType, worldIn, 5);
+        this.stepHeight = 1F;
     }
     
     @Override
     protected void registerGoals() {
         super.registerGoals();
         this.goalSelector.addGoal(0, new SwimGoal(this));
-        this.goalSelector.addGoal(1, new RandomWalkingGoal(this, 0.55D));
+        this.goalSelector.addGoal(1, new RandomWalkingGoal(this, 0.65D));
         this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 0.65D, false));
         this.targetSelector.addGoal(0, new HurtByTargetGoal(this));
         this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<PlayerEntity>(this, PlayerEntity.class, 75, true, true, e -> e.getDistance(this) < 15));
@@ -49,11 +58,54 @@ public class EntityMoose extends EntityAnimalEatsGrassWithTypes {
         this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.6D);
         this.getAttributes().registerAttribute(SharedMonsterAttributes.ATTACK_DAMAGE);
         this.getAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(4.5D);
+        this.getAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE).setBaseValue(0.7D);
     }
 
     @Override
     protected float getWaterSlowDown() {
-        return 0.98F;
+        return 0.9F;
+    }
+    
+    @Override
+    protected void collideWithNearbyEntities() {
+        // prevent pushing
+    }
+    
+    
+
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void doBlockCollisions() {
+        AxisAlignedBB axisalignedbb = this.getBoundingBox();
+        try(
+        BlockPos.PooledMutableBlockPos blockpos$pooledmutableblockpos = BlockPos.PooledMutableBlockPos.retain(axisalignedbb.minX + 0.001D, axisalignedbb.minY + 0.001D, axisalignedbb.minZ + 0.001D);
+        BlockPos.PooledMutableBlockPos blockpos$pooledmutableblockpos1 = BlockPos.PooledMutableBlockPos.retain(axisalignedbb.maxX - 0.001D, axisalignedbb.maxY - 0.001D, axisalignedbb.maxZ - 0.001D);
+        BlockPos.PooledMutableBlockPos blockpos$pooledmutableblockpos2 = BlockPos.PooledMutableBlockPos.retain();) {
+            if(this.world.isAreaLoaded(blockpos$pooledmutableblockpos, blockpos$pooledmutableblockpos1)) {
+                for(int i = blockpos$pooledmutableblockpos.getX(); i <= blockpos$pooledmutableblockpos1.getX(); ++i) {
+                    for(int j = blockpos$pooledmutableblockpos.getY(); j <= blockpos$pooledmutableblockpos1.getY(); ++j) {
+                        for(int k = blockpos$pooledmutableblockpos.getZ(); k <= blockpos$pooledmutableblockpos1.getZ(); ++k) {
+                            blockpos$pooledmutableblockpos2.setPos(i, j, k);
+                            BlockState blockstate = this.world.getBlockState(blockpos$pooledmutableblockpos2);
+
+                            try {
+                                blockstate.onEntityCollision(this.world, blockpos$pooledmutableblockpos2, this);
+                                this.onInsideBlock(blockstate);
+                                if(blockstate.getBlock() == Blocks.LILY_PAD) {
+                                    Block.spawnDrops(blockstate, world, blockpos$pooledmutableblockpos2.toImmutable());
+                                    world.setBlockState(blockpos$pooledmutableblockpos2.toImmutable(), Blocks.AIR.getDefaultState());
+                                }
+                            } catch(Throwable throwable) {
+                                CrashReport crashreport = CrashReport.makeCrashReport(throwable, "Colliding entity with block");
+                                CrashReportCategory crashreportcategory = crashreport.makeCategory("Block being collided with");
+                                CrashReportCategory.addBlockInfo(crashreportcategory, blockpos$pooledmutableblockpos2, blockstate);
+                                throw new ReportedException(crashreport);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 
     protected EntityAIEatGrassCustom provideEatTask() {
