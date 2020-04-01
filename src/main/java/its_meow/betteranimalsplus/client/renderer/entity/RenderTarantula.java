e9a4d1cd0e3dc97aca293cac0230651f992f9d4b
itsmeow <mods.itsmeow@gmail.com>
2020-03-31 22:52:33 -0400

    Make tarantula rotate while climbing

--- src/main/java/its_meow/betteranimalsplus/client/renderer/entity/RenderTarantula.java
+++ src/main/java/its_meow/betteranimalsplus/client/renderer/entity/RenderTarantula.java
@@ -4,6 +4,7 @@ import its_meow.betteranimalsplus.client.model.ModelTarantula;
 import its_meow.betteranimalsplus.client.renderer.entity.layers.LayerEyes;
 import its_meow.betteranimalsplus.common.entity.EntityTarantula;
 import its_meow.betteranimalsplus.init.ModTextures;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
@@ -13,6 +14,14 @@ public class RenderTarantula extends RenderLiving<EntityTarantula> {
     public RenderTarantula(RenderManager rendermanagerIn) {
         super(rendermanagerIn, new ModelTarantula(), 1F);
         this.addLayer(new LayerEyes<EntityTarantula>(this, ModTextures.tarantula_eyes));
+    }
+
+    @Override
+    protected void preRenderCallback(EntityTarantula entity, float p) {
+        if(entity.isBesideClimbableBlock()) {
+            GlStateManager.rotate(-90, 1, 0, 0);
+            GlStateManager.translate(0.0F, 0.75F, -0.5F);
+        }
     }
 
     @Override
