acc2c183afb82e8d202b984badddc2e1b69b764f
itsmeow <mods.itsmeow@gmail.com>
2020-04-18 00:30:06 -0400

    Resolve #156, update buildscripts, set version 1.12.2-9.0.1

--- src/main/java/its_meow/betteranimalsplus/common/entity/EntityFreshwaterEel.java
+++ src/main/java/its_meow/betteranimalsplus/common/entity/EntityFreshwaterEel.java
@@ -1,7 +1,13 @@
 package its_meow.betteranimalsplus.common.entity;
 
+import its_meow.betteranimalsplus.Ref;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@Mod.EventBusSubscriber(modid = Ref.MOD_ID)
 public class EntityFreshwaterEel extends EntityEelBase {
 
     public EntityFreshwaterEel(World worldIn) {
@@ -18,4 +24,15 @@ public class EntityFreshwaterEel extends EntityEelBase {
     protected String getContainerName() {
         return "eel_freshwater";
     }
+
+    @SubscribeEvent
+    public static void onSpawn(LivingSpawnEvent event) {
+        // patched on crappy fix for overspawning
+        if(event.getEntity() instanceof EntityFreshwaterEel) {
+            if(event.getWorld().getEntitiesWithinAABB(EntityFreshwaterEel.class, event.getEntity().getEntityBoundingBox().grow(20)).size() > 5) {
+                event.setResult(Result.DENY);
+            }
+        }
+    }
+
 }
