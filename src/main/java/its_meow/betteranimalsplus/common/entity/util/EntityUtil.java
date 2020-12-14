f17a4b685e4c055038c3d542ba4aaf777bf24e11
itsmeow <mods.itsmeow@gmail.com>
2020-12-13 23:00:01 -0500

    Probably fix null variant baby animals spawning

--- src/main/java/its_meow/betteranimalsplus/common/entity/util/EntityUtil.java
+++ src/main/java/its_meow/betteranimalsplus/common/entity/util/EntityUtil.java
@@ -1,27 +1,32 @@
 package its_meow.betteranimalsplus.common.entity.util;
 
-import dev.itsmeow.imdlib.entity.util.IVariantTypes;
-import its_meow.betteranimalsplus.common.entity.util.abstracts.EntityAnimalWithTypesAndSize;
 import net.minecraft.entity.AgeableEntity;
 import net.minecraft.entity.AgeableEntity.AgeableData;
 import net.minecraft.entity.ILivingEntityData;
 import net.minecraft.entity.SpawnReason;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.logging.log4j.LogManager;
+
+import java.lang.reflect.Field;
 
 public class EntityUtil {
 
+    private static boolean hasFailed =  false;
+
     public static ILivingEntityData childChance(AgeableEntity e, SpawnReason reason, ILivingEntityData livingdata, float chance) {
         if(livingdata != null && livingdata instanceof AgeableData) {
-            int i = ((AgeableData) livingdata).getIndexInGroup();
-            if(livingdata instanceof EntityAnimalWithTypesAndSize.AgeableSizeTypeData) {
-                EntityAnimalWithTypesAndSize.AgeableSizeTypeData d = (EntityAnimalWithTypesAndSize.AgeableSizeTypeData) livingdata;
-                livingdata = new EntityAnimalWithTypesAndSize.AgeableSizeTypeData(d, d.typeData, d.size);
-            } else if(livingdata instanceof IVariantTypes.AgeableTypeData) {
-                IVariantTypes.AgeableTypeData d = (IVariantTypes.AgeableTypeData) livingdata;
-                livingdata = new IVariantTypes.AgeableTypeData(d, d.typeData);
-            } else {
-                livingdata = new AgeableData(chance);
+            try {
+                Field babySpawnProbability = ObfuscationReflectionHelper.findField(AgeableData.class, "field_226256_c_");
+                FieldUtils.removeFinalModifier(babySpawnProbability);
+                babySpawnProbability.set(livingdata, chance);
+            } catch(Exception ex) {
+                if(!hasFailed) {
+                    LogManager.getLogger().error("Error setting baby spawn probability! It will now be 5% (default)");
+                    ex.printStackTrace();
+                    hasFailed = true;
+                }
             }
-            ((AgeableData) livingdata).indexInGroup = i;
         }
         return livingdata;
     }
