8ba54bb607c239dd72514d4f3d23d445340c6e42
itsmeow <itsmeow@itsmeow.dev>
2021-12-12 22:31:17 -0600

    Fix defineId warning suppression mixin

--- forge/src/main/java/dev/itsmeow/betteranimalsplus/mixin/SynchedEntityDataMixin.java
+++ forge/src/main/java/dev/itsmeow/betteranimalsplus/mixin/SynchedEntityDataMixin.java
@@ -13,13 +13,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(SynchedEntityData.class)
 public class SynchedEntityDataMixin {
 
-    @Inject(at = @At(value = "FIELD", target = "Lnet/minecraft/network/syncher/SynchedEntityData;LOGGER:Lorg/apache/logging/log4j/Logger;", ordinal = 1), method = "defineId(Ljava/lang/Class;Lnet/minecraft/network/syncher/EntityDataSerializer;)Lnet/minecraft/network/syncher/EntityDataAccessors;")
+    @Inject(at = @At(value = "FIELD", target = "Lnet/minecraft/network/syncher/SynchedEntityData;LOGGER:Lorg/apache/logging/log4j/Logger;", ordinal = 1), method = "defineId(Ljava/lang/Class;Lnet/minecraft/network/syncher/EntityDataSerializer;)Lnet/minecraft/network/syncher/EntityDataAccessor;")
     private static <T> void defineId(Class<? extends Entity> class_, EntityDataSerializer<T> entityDataSerializer, CallbackInfoReturnable<EntityDataAccessor<T>> callback) throws ClassNotFoundException {
         if(IContainerEntity.class.isAssignableFrom(class_))
             throw new ClassNotFoundException();
     }
 
-    @Inject(at = @At(value = "FIELD", target = "Lnet/minecraft/network/syncher/SynchedEntityData;LOGGER:Lorg/apache/logging/log4j/Logger;", ordinal = 2), method = "defineId(Ljava/lang/Class;Lnet/minecraft/network/syncher/EntityDataSerializer;)Lnet/minecraft/network/syncher/EntityDataAccessors;")
+    @Inject(at = @At(value = "FIELD", target = "Lnet/minecraft/network/syncher/SynchedEntityData;LOGGER:Lorg/apache/logging/log4j/Logger;", ordinal = 2), method = "defineId(Ljava/lang/Class;Lnet/minecraft/network/syncher/EntityDataSerializer;)Lnet/minecraft/network/syncher/EntityDataAccessor;")
     private static <T> void defineId2(Class<? extends Entity> class_, EntityDataSerializer<T> entityDataSerializer, CallbackInfoReturnable<EntityDataAccessor<T>> callback) throws ClassNotFoundException {
         if(IContainerEntity.class.isAssignableFrom(class_))
             throw new ClassNotFoundException();
