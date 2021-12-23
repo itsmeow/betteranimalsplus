ff32fcdce57557d382ae44bb7d43b9280c15be8a
itsmeow <itsmeow@itsmeow.dev>
2021-12-23 04:06:24 -0500

    Fix being able to equip two capes

--- fabric/src/main/java/dev/itsmeow/betteranimalsplus/compat/trinkets/TrinketsModCompat.java
+++ fabric/src/main/java/dev/itsmeow/betteranimalsplus/compat/trinkets/TrinketsModCompat.java
@@ -2,12 +2,16 @@ package dev.itsmeow.betteranimalsplus.compat.trinkets;
 
 import dev.emi.trinkets.api.TrinketsApi;
 import dev.itsmeow.betteranimalsplus.common.item.ItemCape;
+import dev.itsmeow.betteranimalsplus.init.ModItems;
+import me.shedaniel.architectury.registry.RegistrySupplier;
 import net.minecraft.world.entity.player.Player;
+
+import java.util.stream.Collectors;
 
 public class TrinketsModCompat {
 
     public static void init() {
-        ItemCape.can_equip = (stack, armorType, entity) -> TrinketsApi.getTrinketsInventory((Player) entity).countItem(stack.getItem()) == 0;
+        ItemCape.can_equip = (stack, armorType, entity) -> !TrinketsApi.getTrinketsInventory((Player) entity).hasAnyOf(ModItems.getModeledArmor().values().stream().map(RegistrySupplier::getOrNull).collect(Collectors.toSet()));
     }
 
 }
