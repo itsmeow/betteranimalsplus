ec4ac461c0db713b88ebe097d977176ce2ee9b2d
itsmeow <itsmeow@itsmeow.dev>
2021-12-28 03:47:15 -0500

    Tweak mixins to allow dev dependencies on the mod

--- fabric/src/main/java/dev/itsmeow/betteranimalsplus/mixin/fabric/EntityMixin.java
+++ fabric/src/main/java/dev/itsmeow/betteranimalsplus/mixin/fabric/EntityMixin.java
@@ -17,11 +17,8 @@ import java.util.ArrayList;
 @Mixin(Entity.class)
 public class EntityMixin {
 
-    @Shadow
-    public Level level;
-
     @Unique
-    protected ArrayList<ItemEntity> drops = null;
+    public ArrayList<ItemEntity> drops = null;
 
     @Inject(at = @At(value = "FIELD", target = "Lnet/minecraft/world/entity/Entity;level:Lnet/minecraft/world/level/Level;", ordinal = 2), method = "spawnAtLocation(Lnet/minecraft/world/item/ItemStack;F)Lnet/minecraft/world/entity/item/ItemEntity;", cancellable = true, locals = LocalCapture.CAPTURE_FAILEXCEPTION)
     private void spawnAtLocation(ItemStack itemStack, float f, CallbackInfoReturnable<ItemEntity> callback, ItemEntity itemEntity) {
