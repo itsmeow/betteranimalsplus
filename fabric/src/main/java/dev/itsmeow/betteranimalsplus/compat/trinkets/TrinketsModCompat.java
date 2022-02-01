baceb833a92352715d309ecdd45d08ae808a47a2
itsmeow <itsmeow@itsmeow.dev>
2022-02-01 01:16:27 -0500

    Fix cape predicate

--- fabric/src/main/java/dev/itsmeow/betteranimalsplus/compat/trinkets/TrinketsModCompat.java
+++ fabric/src/main/java/dev/itsmeow/betteranimalsplus/compat/trinkets/TrinketsModCompat.java
@@ -13,10 +13,10 @@ public class TrinketsModCompat {
     public static void init() {
         ItemCape.can_equip = (stack, armorType, entity) -> !TrinketsApi.getTrinketComponent((LivingEntity) entity).get().isEquipped(s -> s.getItem() instanceof ItemCape);
         TrinketsApi.registerTrinketPredicate(new ResourceLocation(Ref.MOD_ID, "cape_allowed"), (stack, slot, entity) -> {
-            if (stack.getItem() instanceof ItemCape && entity.getItemBySlot(EquipmentSlot.CHEST).getItem() instanceof ItemCape) {
+            if (stack.getItem() instanceof ItemCape && (entity.getItemBySlot(EquipmentSlot.CHEST).getItem() instanceof ItemCape || !ItemCape.can_equip.canEquip(stack, EquipmentSlot.CHEST, entity))) {
                 return TriState.FALSE;
             }
-            return TriState.TRUE;
+            return TriState.DEFAULT;
         });
     }
 
