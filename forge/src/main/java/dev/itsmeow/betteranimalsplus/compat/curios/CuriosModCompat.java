a845f1fb2e6d457525d82ae4e34629420ef05f60
itsmeow <itsmeow@itsmeow.dev>
2022-12-20 20:27:09 -0500

    Fix crash when putting cape on non-player with Curios (Fixes #368)

--- forge/src/main/java/dev/itsmeow/betteranimalsplus/compat/curios/CuriosModCompat.java
+++ forge/src/main/java/dev/itsmeow/betteranimalsplus/compat/curios/CuriosModCompat.java
@@ -34,7 +34,7 @@ public class CuriosModCompat {
     public static void subscribe(IEventBus modBus) {
         modBus.addListener(CuriosModCompat::interModEnqueue);
         MinecraftForge.EVENT_BUS.register(CuriosModCompat.class);
-        ItemCape.can_equip = (stack, armorType, entity) -> !CuriosApi.getCuriosHelper().findEquippedCurio(s -> s.getItem() instanceof ItemCape, (Player) entity).isPresent();
+        ItemCape.can_equip = (stack, armorType, entity) -> !(entity instanceof Player) || !CuriosApi.getCuriosHelper().findEquippedCurio(s -> s.getItem() instanceof ItemCape, (Player) entity).isPresent();
         ClassLoadHacks.runIf(Platform.getEnv() == Dist.CLIENT, () -> () -> {
             modBus.addListener((FMLClientSetupEvent event) -> {
                 ModItems.getModeledArmor().forEach((k, item) -> {
