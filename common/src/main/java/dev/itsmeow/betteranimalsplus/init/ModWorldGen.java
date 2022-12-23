af9d2e6e176f994a360e5219ca25e2c9a52e6687
itsmeow <itsmeow@itsmeow.dev>
2022-12-23 07:25:47 -0500

    Finish most of 1.19.3, re-add trillium generation

--- common/src/main/java/dev/itsmeow/betteranimalsplus/init/ModWorldGen.java
+++ common/src/main/java/dev/itsmeow/betteranimalsplus/init/ModWorldGen.java
@@ -2,68 +2,30 @@ package dev.itsmeow.betteranimalsplus.init;
 
 import dev.architectury.registry.level.biome.BiomeModifications;
 import dev.itsmeow.betteranimalsplus.Ref;
-import net.minecraft.core.Direction;
-import net.minecraft.core.Holder;
-import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
-import net.minecraft.util.random.SimpleWeightedRandomList;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.block.HorizontalDirectionalBlock;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.GenerationStep;
-import net.minecraft.world.level.levelgen.Heightmap;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
-import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration;
-import net.minecraft.world.level.levelgen.feature.stateproviders.WeightedStateProvider;
-import net.minecraft.world.level.levelgen.placement.*;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
-import java.util.List;
 import java.util.function.Consumer;
 
 public class ModWorldGen {
 
-    public static ConfiguredFeature<RandomPatchConfiguration, ?> TRILLIUM_CF = null;
-    public static PlacedFeature TRILLIUM_PF = null;
-    public static PlacedFeature TRILLIUM_DENSE_PF = null;
+    public static final ResourceKey<PlacedFeature> TRILLIUM = ResourceKey.create(Registries.PLACED_FEATURE, new ResourceLocation(Ref.MOD_ID, "trillium"));
+    public static final ResourceKey<PlacedFeature> TRILLIUM_DENSE = ResourceKey.create(Registries.PLACED_FEATURE, new ResourceLocation(Ref.MOD_ID, "trillium_dense"));
 
     public static void init(Consumer<Runnable> enqueue) {
-        // TODO
-        // Mojang don't break worldgen code challenge (impossible)
-        /*TRILLIUM_CF = new ConfiguredFeature(Feature.RANDOM_PATCH, FeatureUtils.simplePatchConfiguration(Feature.SIMPLE_BLOCK, new SimpleBlockConfiguration(
-                        new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder()
-                                .add(ModBlocks.TRILLIUM.get().defaultBlockState().setValue(HorizontalDirectionalBlock.FACING, Direction.NORTH), 1)
-                                .add(ModBlocks.TRILLIUM.get().defaultBlockState().setValue(HorizontalDirectionalBlock.FACING, Direction.EAST), 1)
-                                .add(ModBlocks.TRILLIUM.get().defaultBlockState().setValue(HorizontalDirectionalBlock.FACING, Direction.SOUTH), 1)
-                                .add(ModBlocks.TRILLIUM.get().defaultBlockState().setValue(HorizontalDirectionalBlock.FACING, Direction.WEST), 1))),
-                List.of(), 64));
-        TRILLIUM_PF = new PlacedFeature(Holder.direct(TRILLIUM_CF), List.of(
-                RarityFilter.onAverageOnceEvery(16),
-                InSquarePlacement.spread(),
-                HeightmapPlacement.onHeightmap(Heightmap.Types.MOTION_BLOCKING),
-                BiomeFilter.biome()));
-        TRILLIUM_DENSE_PF = new PlacedFeature(Holder.direct(TRILLIUM_CF), List.of(
-                RarityFilter.onAverageOnceEvery(4),
-                InSquarePlacement.spread(),
-                HeightmapPlacement.onHeightmap(Heightmap.Types.MOTION_BLOCKING),
-                BiomeFilter.biome()));
-        enqueue.accept(() -> {
-            Registry.register(Registries.CONFIGURED_FEATURE, new ResourceLocation(Ref.MOD_ID, "trillium"), TRILLIUM_CF);
-            Registry.register(Registries.PLACED_FEATURE, new ResourceLocation(Ref.MOD_ID, "trillium"), TRILLIUM_PF);
-            Registry.register(Registries.PLACED_FEATURE, new ResourceLocation(Ref.MOD_ID, "trillium_dense"), TRILLIUM_DENSE_PF);
-        });
         BiomeModifications.addProperties(
                 ctx -> isSwamp(ctx) && !isDenseSwamp(ctx),
-                (ctx, mutable) -> mutable.getGenerationProperties().addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, TRILLIUM_PF)
+                (ctx, mutable) -> mutable.getGenerationProperties().addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, TRILLIUM)
         );
         BiomeModifications.addProperties(
                 ctx -> isSwamp(ctx) && isDenseSwamp(ctx),
-                (ctx, mutable) -> mutable.getGenerationProperties().addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, TRILLIUM_DENSE_PF)
-        );*/
+                (ctx, mutable) -> mutable.getGenerationProperties().addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, TRILLIUM_DENSE)
+        );
     }
 
     private static boolean isSwamp(BiomeModifications.BiomeContext ctx) {
--- common/src/main/java/dev/itsmeow/betteranimalsplus/mixin/ClientPacketListenerMixin.java
+++ common/src/main/java/dev/itsmeow/betteranimalsplus/mixin/ClientPacketListenerMixin.java
@@ -1,20 +1,18 @@
 package dev.itsmeow.betteranimalsplus.mixin;
 
 import dev.itsmeow.betteranimalsplus.client.dumb.DeveloperRenderThing;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.network.chat.Component;
+import net.minecraft.client.multiplayer.ClientPacketListener;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LocalPlayer.class)
-public class LocalPlayerMixin {
-    // TODO
-    /*@Inject(method = "command(Ljava/lang/String;Lnet/minecraft/network/chat/Component;)V", at = @At("HEAD"), cancellable = true)
-    public void command(String message, Component component, CallbackInfo callback) {
+@Mixin(ClientPacketListener.class)
+public class ClientPacketListenerMixin {
+
+    @Inject(method = "sendCommand(Ljava/lang/String;)V", at = @At("HEAD"), cancellable = true)
+    public void sendCommand(String message, CallbackInfo callback) {
         if (DeveloperRenderThing.chat("/" + message).isFalse())
             callback.cancel();
-    }*/
-
+    }
 }
